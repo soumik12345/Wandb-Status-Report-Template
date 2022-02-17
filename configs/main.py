2aa3bbc306cb1f1b3c25a1cfe3ca9dafc0578cf9
soumik12345 <19soumik.rakshit96@gmail.com>
2022-02-17 19:09:03 +0530

    updated imports

--- configs/main.py
+++ configs/main.py
@@ -1,8 +1,45 @@
 import ml_collections
+from fastai.vision.all import *
 
-from wandb_configs import get_config as get_wandb_configs
-from experiment import get_config as get_experiment_configs
-from loss import get_config as get_loss_mappings
+
+def get_wandb_configs() -> ml_collections.ConfigDict:
+    config = ml_collections.ConfigDict()
+
+    config.project = "CamVid"
+    config.entity = "av-demo"
+    config.job_type = "sweep"
+    config.artifact_id = "camvid-dataset:v0"
+
+    return config
+
+
+def get_experiment_configs() -> ml_collections.ConfigDict:
+    config = ml_collections.ConfigDict()
+
+    config.seed = 123
+    config.batch_size = 8
+    config.image_height = 720
+    config.image_width = 960
+    config.image_resize_factor = 4
+    config.validation_split = 0.2
+    config.backbone = "mobilenetv2_100"
+    config.hidden_dims = 256
+    config.num_epochs = 5
+    config.loss_function = "categorical_cross_entropy"
+    config.learning_rate = 1e-3
+    config.fit = "fit"
+
+    return config
+
+
+def get_loss_mappings() -> ml_collections.ConfigDict:
+    config = ml_collections.ConfigDict()
+
+    config.categorical_cross_entropy = (CrossEntropyLossFlat,)
+    config.focal = (FocalLossFlat,)
+    config.dice = DiceLoss
+
+    return config
 
 
 def get_config() -> ml_collections.ConfigDict:
