10536c9af910d9c2fa18d9deda411e7cd243ebcf
soumik12345 <19soumik.rakshit96@gmail.com>
2022-02-18 01:28:42 +0530

    attempted to reduce gpu memory footprint for sweeps

--- sweep.py
+++ sweep.py
@@ -1,5 +1,5 @@
-from absl import app, flags
 import wandb
+from absl import app, flags
 
 from functools import partial
 
@@ -11,14 +11,12 @@ from train import train_fn
 FLAGS = flags.FLAGS
 config_flags.DEFINE_config_file("experiment_configs")
 
+
 def main(_):
     config = FLAGS.experiment_configs
     sweep_configs = {
         "method": config.sweep_method,
-        "metric": {
-            "name": config.sweep_metric_name,
-            "goal": config.sweep_goal
-        },
+        "metric": {"name": config.sweep_metric_name, "goal": config.sweep_goal},
         "early_terminate": {
             "type": config.early_terminate_type,
             "min_iter": config.early_terminate_min_iter,
@@ -38,12 +36,8 @@ def main(_):
                 ]
             },
             "loss_function": {"values": ["categorical_cross_entropy", "focal", "dice"]},
-            "learning_rate": {"distribution": "uniform",
-                              "min": 1e-5,
-                              "max": 1e-2},
-            "weight_decay":{"distribution": "uniform",
-                            "min": 0.,
-                            "max": 0.05}
+            "learning_rate": {"distribution": "uniform", "min": 1e-5, "max": 1e-2},
+            "weight_decay": {"distribution": "uniform", "min": 0.0, "max": 0.05},
         },
     }
     sweep_id = wandb.sweep(
@@ -51,9 +45,7 @@ def main(_):
         project=config.wandb_configs.project,
         entity=config.wandb_configs.entity,
     )
-    wandb.agent(
-        sweep_id, function=partial(train_fn, config), count=config.sweep_count
-    )
+    wandb.agent(sweep_id, function=partial(train_fn, config), count=config.sweep_count)
 
 
 if __name__ == "__main__":
--- train.py
+++ train.py
@@ -1,20 +1,11 @@
 import wandb
-from functools import partial
-from fastai.vision.all import *
-
-from absl import app
-from absl import flags
-
 import ml_collections
-from ml_collections.config_flags import config_flags
+from fastai.vision.all import *
 
 from segmentation.camvid_utils import *
 from segmentation.train_utils import *
 from segmentation.metrics import *
-
-
-# FLAGS = flags.FLAGS
-# config_flags.DEFINE_config_file("experiment_configs")
+from configs import get_config
 
 
 def train_fn(configs: ml_collections.ConfigDict):
@@ -22,12 +13,9 @@ def train_fn(configs: ml_collections.ConfigDict):
     experiment_configs = configs.experiment_configs
     loss_alias_mappings = configs.loss_mappings
     inference_config = configs.inference
-    
-    # set seeds!
-    
-    
+
     run = wandb.init(
-        name = wandb_configs.name,
+        name=wandb_configs.name,
         project=wandb_configs.project,
         entity=wandb_configs.entity,
         job_type=wandb_configs.job_type,
@@ -56,18 +44,22 @@ def train_fn(configs: ml_collections.ConfigDict):
     )
 
     if wandb.config.fit == "fit":
-        learner.fit_one_cycle(wandb.config.num_epochs, 
-                              wandb.config.learning_rate,
-                              wd=wandb.config.weight_decay)
+        learner.fit_one_cycle(
+            wandb.config.num_epochs,
+            wandb.config.learning_rate,
+            wd=wandb.config.weight_decay,
+        )
     else:
-        learner.fine_tune(wandb.config.num_epochs, 
-                          wandb.config.learning_rate,
-                          wd=wandb.config.weight_decay)
+        learner.fine_tune(
+            wandb.config.num_epochs,
+            wandb.config.learning_rate,
+            wd=wandb.config.weight_decay,
+        )
 
     wandb.log(
         {f"Predictions_Table": table_from_dl(learner, learner.dls.valid, class_labels)}
     )
-    
+
     # store model checkpoints and JIT
     save_model_to_artifacts(
         learner.model,
@@ -81,22 +73,22 @@ def train_fn(configs: ml_collections.ConfigDict):
             "class_labels": class_labels,
         },
     )
-    
+
     ## Inference benchmark
     model_file = f"Unet_{wandb.config.backbone}_traced.pt"
+    del learner
     torch.cuda.empty_cache()
-    inference_time = benchmark_inference_time(model_file,
-                        batch_size=inference_config.batch_size,
-                        image_shape=(wandb.config.image_height,
-                                     wandb.config.image_width),
-                        num_warmup_iters=inference_config.warmup,
-                        num_iter=inference_config.num_iter,
-                        resize_factor=inference_config.resize_factor
-                        )
-    wandb.log({"inference_time":inference_time})
+    inference_time = benchmark_inference_time(
+        model_file,
+        batch_size=inference_config.batch_size,
+        image_shape=(wandb.config.image_height, wandb.config.image_width),
+        num_warmup_iters=inference_config.warmup,
+        num_iter=inference_config.num_iter,
+        resize_factor=inference_config.resize_factor,
+    )
+    wandb.log({"inference_time": inference_time})
 
-    
-from configs import get_config
-if __name__=="__main__":
+
+if __name__ == "__main__":
     cfg = get_config()
     train_fn(cfg)
