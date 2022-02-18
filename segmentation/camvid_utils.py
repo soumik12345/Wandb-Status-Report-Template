8b4f3b74f8f12f5b2f3c689db95ac0c362ca5a41
soumik12345 <19soumik.rakshit96@gmail.com>
2022-02-18 19:52:52 +0530

    added utils for camvid

--- segmentation/camvid_utils.py
+++ segmentation/camvid_utils.py
@@ -1,10 +1,65 @@
 import wandb
-from typing import Tuple
+from typing import Tuple, Dict
 from fastai.vision.all import *
 
 
 def label_func(fn):
     return fn.parent.parent / "labels" / f"{fn.stem}_P{fn.suffix}"
+
+
+def get_frequency_distribution(mask_data, class_labels):
+    (unique, counts) = np.unique(mask_data, return_counts=True)
+    unique = list(unique)
+    counts = list(counts)
+    frequency_dict = {}
+    for _class in class_labels.keys():
+        if _class in unique:
+            frequency_dict[class_labels[_class]] = counts[unique.index(_class)]
+        else:
+            frequency_dict[class_labels[_class]] = 0
+    return frequency_dict
+
+
+def log_dataset(project: str, entity: str, artifact_id: str, class_labels: Dict):
+    with wandb.init(
+        project=project, name="visualize_camvid", entity=entity, job_type="data_viz"
+    ):
+        artifact = wandb.use_artifact(artifact_id, type="dataset")
+        artifact_dir = artifact.download()
+
+        table_data = []
+        image_files = get_image_files(Path(artifact_dir) / "images")
+        labels = [str(class_labels[_lab]) for _lab in list(class_labels)]
+
+        print("Creating Table...")
+        for image_file in progress_bar(image_files):
+            image = np.array(Image.open(image_file))
+            mask_data = np.array(Image.open(label_func(image_file)))
+            frequency_distribution = get_frequency_distribution(mask_data)
+            table_data.append(
+                [
+                    str(image_file.name),
+                    wandb.Image(image),
+                    wandb.Image(
+                        image,
+                        masks={
+                            "predictions": {
+                                "mask_data": mask_data,
+                                "class_labels": class_labels,
+                            }
+                        },
+                    ),
+                ]
+                + [frequency_distribution[_lab] for _lab in labels]
+            )
+        wandb.log(
+            {
+                "CamVid_Dataset": wandb.Table(
+                    data=table_data,
+                    columns=["File_Name", "Images", "Segmentation_Masks"] + labels,
+                )
+            }
+        )
 
 
 def get_dataloader(
